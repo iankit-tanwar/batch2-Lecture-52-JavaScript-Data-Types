2cd7e709552febe5d4c689ef0630e4c9b59d1000
iankit-tanwar <tankitofficial01.gmail.com>
2023-06-17 23:07:39 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+// Js object are used to stored some imformation
+let student1 = {
+    // 1 property : value
+    name :'Sonu',
+    surname: 'Sharma'
+    
+    //2
+} // JS Object
+
+// 1 way to see output
+//object:property
+
+console.log(`hello `+ student1.name +' '+student1.surname);
